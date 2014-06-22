534219b61d82f12c800c1f685e1c052cf8332a77
christoferpeterson <christofer.peterson@markit.com>
2014-06-22 14:27:52 -0600

    Cultured Dates
    Added culture support,
    Updated some date formatting,
    Added new date formats,
    Added Ordinal number extension

---
+++ Extensions/NumberExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Translucent.Extensions
+{
+	public static class NumberExtensions
+	{
+		public static string Ordinal(this int number)
+		{
+			var work = number.ToString();
+			if (number == 11 || number == 12 || number == 13)
+				return work + "th";
+			switch (number % 10)
+			{
+				case 1: work += "st"; break;
+				case 2: work += "nd"; break;
+				case 3: work += "rd"; break;
+				default: work += "th"; break;
+			}
+			return work;
+		}
+	}
+}
--- FormatData/Format.cs
+++ FormatData/Format.cs
@@ -1,23 +1,64 @@
 ﻿using System;
+using System.Web;
+using System.Globalization;
+using Translucent.Extensions;
 
 namespace Translucent.FormatData
 {
 	public static partial class Format
 	{
+		static CultureInfo _defaultCulture
+		{
+			get
+			{
+				var culture = new CultureInfo("en-US");
+
+				culture.DateTimeFormat.PMDesignator = "pm";
+				culture.DateTimeFormat.AMDesignator = "am";
+
+				culture.NumberFormat.PercentPositivePattern = 1;
+				culture.NumberFormat.PercentNegativePattern = 1;
+
+				return culture;
+			}
+		}
+
+		/// <summary>Get and set the culture for the current request
+		/// </summary>
+		public static CultureInfo Culture
+		{
+			get
+			{
+				CultureInfo output;
+
+				output = (HttpContext.Current.Items["CultureInfo"] as CultureInfo) ?? _defaultCulture;
+
+				return output;
+			}
+			set
+			{
+				HttpContext.Current.Items["CultureInfo"] = value;
+			}
+		}
+
+		#region DateTime
+
 		public static string TimeStamp(DateTime? date)
 		{
-			string value;
+			string value = "";
 
 			if (date.HasValue)
 			{
 				if (date.Value.Date == DateTime.Today)
-					value = "Today at " + date.Value.ToString("h:mm:ss tt");
+					value = "Today at ";
 				else if (date.Value.Date == DateTime.Today.AddDays(-1))
-					value = "Yesterday at " + date.Value.ToString("h:mm:ss tt");
+					value = "Yesterday at ";
 				else if (date.Value.Date == DateTime.Today.AddDays(1))
-					value = "Tomorrow at " + date.Value.ToString("h:mm:ss tt");
+					value = "Tomorrow at ";
 				else
-					value = date.Value.ToString();
+					value += "";
+
+				value += date.Value.ToString(@"h:mmtt \MT", Culture);
 			}
 			else
 			{
@@ -26,5 +67,62 @@ namespace Translucent.FormatData
 
 			return value;
 		}
+
+		public static string ShortDateTime(DateTime? date)
+		{
+			string value = "";
+
+			if(date.HasValue)
+			{
+				if(date.Value.Date == DateTime.Today)
+				{
+					value = date.Value.ToString(@"h:mmtt \MT", Culture);
+				}
+				else
+				{
+					value = date.Value.ToString(@"M/d/yyyy h:mmtt \MT", Culture);
+				}
+			}
+			else
+			{
+				value = "No date";
+			}
+
+			return value;
+		}
+
+		public static string FullDate(DateTime? date)
+		{
+			string value = "";
+
+			if(date.HasValue)
+			{
+				if(date.Value.Date == DateTime.Today)
+				{
+					value = "Today, ";
+				}
+				else if(date.Value.Date == DateTime.Today.AddDays(-1))
+				{
+					value = "Yesterday, ";
+				}
+				else if(date.Value.Date == DateTime.Today.AddDays(1))
+				{
+					value = "Tomorrow, ";
+				}
+				else
+				{
+					value = date.Value.ToString("dddd") + ", ";
+				}
+				value += String.Format("{0} {1}, {2}", date.Value.ToString("MMMM", Culture), date.Value.Day.Ordinal(), date.Value.ToString("yyyy", Culture));
+			}
+			else
+			{
+				value = "No date";
+			}
+
+			return value;
+		}
+
+		#endregion
 	}
 }
