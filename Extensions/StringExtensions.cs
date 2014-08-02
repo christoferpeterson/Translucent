ff49d7b1da7f8e41f11011ab5e2a7d09b37380ec
christoferpeterson <christofer.peterson@markit.com>
2014-08-02 16:38:55 -0600

    Massive Format Update + New Extensions
    HtmlHelperExtensions for custom editor names,
    changed namespace of number extensions,
    Added valuable string extensions: StripHtml and Truncate,
    TimeZoneInfo extension to display the correct name,
    Reworked the format class and added many new formats,
    Normalized format options and added some error handling,
    Renamed params to ParamsAttribute

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+public static class StringExtensions
+{
+	public static string StripHtml(this string source)
+	{
+		return Regex.Replace(source, "<.*?>", string.Empty);
+	}
+
+	/// <summary>Truncate a string to a word specified length
+	/// </summary>
+	/// <param name="s">extension method</param>
+	/// <param name="maxLength">maximum number of characters</param>
+	/// <returns>a truncated string of length maxLength - 3, ellipsis (...) appended</returns>
+	public static string Truncate(this string s, uint maxLength)
+	{
+		return s.Truncate(maxLength, "...");
+	}
+
+	/// <summary>Truncate a string to a word specified length
+	/// </summary>
+	/// <param name="s">extension method</param>
+	/// <param name="maxLength">maximum number of characters</param>
+	/// <param name="append">the string to attached to the end of the truncation</param>
+	/// <returns>a truncated string of length maxLength - 3, ellipsis (...) appended</returns>
+	public static string Truncate(this string s, uint maxLength, string append)
+	{
+		return s.Truncate(maxLength, append, new char[] { ' ', ',', '/', '\\', '\'', '\"', '.', '?', '(', '[', '{', '<' });
+	}
+
+	/// <summary>Truncate a string to specified length
+	/// </summary>
+	/// <param name="s">extension method</param>
+	/// <param name="maxLength">maximum number of characters</param>
+	/// <param name="append">the string to attached to the end of the truncation</param>
+	/// <param name="truncateChars">an array of characters to be removed form the end of the string</param>
+	/// <returns>a truncated string of length maxLength - append length, with a specified append</returns>
+	public static string Truncate(this string s, uint maxLength, string append, char[] truncateChars)
+	{
+		if (s == null || s.Trim().Length <= maxLength || truncateChars == null)
+			return s;
+
+		int index = s.Trim().LastIndexOfAny(truncateChars);
+
+		// find the index of where to truncate using the provided list of characters
+		while ((index + append.Length) > maxLength)
+			index = s.Substring(0, index).Trim().LastIndexOfAny(truncateChars);
+
+		// if the index is > 0, truncate the string and append
+		if (index > 0)
+			return s.Substring(0, index) + append;
+
+		// if the index is not 0, truncate regardless of provided characters
+		return s.Substring(0, (int)maxLength - append.Length).Trim() + append;
+	}
+
+	/// <summary>Trim the trailing characters of a string based on input
+	/// </summary>
+	/// <param name="source">the string to trim</param>
+	/// <param name="truncateChars">the characters to remove, must be all inclusive</param>
+	/// <returns>The string with offending, trailing characters removed</returns>
+	private static string RemoveTrailingCharacters(string source, char[] truncateChars)
+	{
+		if(!String.IsNullOrEmpty(source) && truncateChars != null && truncateChars.Contains(source[source.Length]))
+		{
+			return RemoveTrailingCharacters(source.Substring(0, source.Length - 1), truncateChars);
+		}
+
+		return source;
+	}
+}
---
+++ FormatData/FormatDateTime.cs
@@ -0,0 +1,167 @@
+﻿using System;
+
+namespace Translucent.FormatData
+{
+	public partial class Format
+	{
+		/// <summary>Convert a DateTime from utc to the current timezone
+		/// </summary>
+		/// <param name="dt">the datetime to convert (must be utc)</param>
+		/// <returns>adjusted datetime according to current value of Format.TimeZone</returns>
+		public static DateTime? DateTimeFromUTC(DateTime? dt)
+		{
+			// verify date has a value before converting
+			var valid = IsValid(dt);
+			if (valid == DataState.valid)
+				return TimeZoneInfo.ConvertTimeFromUtc(dt.Value, Format.Options.TimeZone);
+			return null;
+		}
+
+		/// <summary>Format a datetime into an Rfc822 compliant string
+		/// </summary>
+		/// <param name="dt">the datetime to format</param>
+		/// <returns>"r"</returns>
+		public static string Rfc822(DateTime? dt)
+		{
+			var valid = IsValid(dt);
+			if (valid == DataState.valid)
+			{
+				return dt.Value.ToString("r", Format.Options.Culture);
+			}
+
+			return ErrorString(valid);
+		}
+
+		/// <summary>Creates a timestamp from a date time
+		/// </summary>
+		/// <param name="dt">the date time to convert</param>
+		/// <returns>Relative day</returns>
+		public static string TimeStamp(DateTime? dt)
+		{
+			return TimeStamp(dt, false);
+		}
+
+		/// <summary>Creates a timestamp from a date time
+		/// </summary>
+		/// <param name="dt">the date time to convert</param>
+		/// <param name="includeDate">include the month, day, year in the timestamp</param>
+		/// <returns>Relative day</returns>
+		public static string TimeStamp(DateTime? dt, bool includeDate)
+		{
+			string value = "";
+			var valid = IsValid(dt);
+			if (valid == DataState.valid)
+			{
+				if (dt.Value.Date == DateTime.Today)
+					value = "Today at ";
+				else if (dt.Value.Date == DateTime.Today.AddDays(-1))
+					value = "Yesterday at ";
+				else if (dt.Value.Date == DateTime.Today.AddDays(1))
+					value = "Tomorrow at ";
+				else if (includeDate)
+					value += ShortDate(dt) + " at ";
+
+				value += ShortTime(dt);
+			}
+			else
+			{
+				value = "never";
+			}
+
+			return value;
+		}
+
+		/// <summary>Convert a nullable datetime into a short formatted string
+		/// </summary>
+		/// <param name="dt">the date to convert</param>
+		/// <returns>If today: "h:mmtt", else @"M/d/yyyy h:mmtt"</returns>
+		public static string ShortDateTime(DateTime? dt)
+		{
+			string value = "";
+
+			var valid = IsValid(dt);
+			if (valid == DataState.valid)
+			{
+				if (dt.Value.Date == DateTime.Today)
+				{
+					value = ShortTime(dt);
+				}
+				else
+				{
+					value = String.Format("{0} {1}", ShortDate(dt), ShortTime(dt));
+				}
+			}
+			else
+			{
+				value = "No date";
+			}
+
+			return value;
+		}
+
+		/// <summary>Convert a datetime into a short time
+		/// </summary>
+		/// <param name="dt">the time to convert</param>
+		/// <returns>@"h:mmtt"</returns>
+		public static string ShortTime(DateTime? dt)
+		{
+			var valid = IsValid(dt);
+			if (valid == DataState.valid)
+			{
+				return dt.Value.ToString(@"h:mmtt", Format.Options.Culture);
+			}
+			return "";
+		}
+
+		/// <summary>Format a short date
+		/// </summary>
+		/// <param name="dt">the date to convert</param>
+		/// <returns>@"M/d/yyyy"</returns>
+		public static string ShortDate(DateTime? dt)
+		{
+			var valid = IsValid(dt);
+			if (valid == DataState.valid)
+			{
+				return dt.Value.ToString(@"M/d/yyyy", Format.Options.Culture);
+			}
+
+			return "";
+		}
+
+		/// <summary>Convert a nullable datetime into a full formatted string
+		/// </summary>
+		/// <param name="dt">the date to convert</param>
+		/// <returns>Includes relative day string or "dddd", "MMMM" "yyyy"</returns>
+		public static string FullDate(DateTime? dt)
+		{
+			string value = "";
+			var valid = IsValid(dt);
+			if (valid == DataState.valid)
+			{
+				if (dt.Value.Date == DateTime.Today)
+				{
+					value = "Today, ";
+				}
+				else if (dt.Value.Date == DateTime.Today.AddDays(-1))
+				{
+					value = "Yesterday, ";
+				}
+				else if (dt.Value.Date == DateTime.Today.AddDays(1))
+				{
+					value = "Tomorrow, ";
+				}
+				else
+				{
+					value = dt.Value.ToString("dddd") + ", ";
+				}
+				value += String.Format("{0} {1}, {2}", dt.Value.ToString("MMMM", Format.Options.Culture), dt.Value.Day.Ordinal(), dt.Value.ToString("yyyy", Format.Options.Culture));
+			}
+			else
+			{
+				value = "No date";
+			}
+
+			return value;
+		}
+	}
+}
