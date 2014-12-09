d41806787efaa2e7c9e53b63576850f99ad995e3
christoferpeterson <christofer.peterson@markit.com>
2014-12-09 16:43:03 -0700

    Currency Format
    - added currency format

--- FormatData/FormatNumber.cs
+++ FormatData/FormatNumber.cs
@@ -33,5 +33,28 @@
 
 			return ErrorString(valid);
 		}
+
+		public static string Currency(double? n, int? precision = 2, bool color = false)
+		{
+			var valid = IsValid(n);
+			if (valid == DataState.valid)
+			{
+				var culture = Options.Culture;
+				if (precision.HasValue)
+					culture.NumberFormat.NumberDecimalDigits = precision.Value;
+
+				var output = n.Value.ToString("c", culture);
+
+				if (color)
+				{
+					var colorClass = n.Value < 0 ? Options.NegativeColorClass : n.Value > 0 ? Options.PositiveColorClass : Options.NeutralColorClass;
+					output = string.Format("<span class=\"{0}\">{1}</span>", colorClass, output);
+				}
+
+				return output;
+			}
+
+			return ErrorString(valid);
+		}
 	}
 }
