f5175c1784c6c5cd526d7974cf93d25e1469df9a
christoferpeterson <christofer.peterson@markit.com>
2015-08-22 16:09:11 -0600

    Percent Formatting

--- FormatData/FormatNumber.cs
+++ FormatData/FormatNumber.cs
@@ -34,6 +34,29 @@
 			return ErrorString(valid);
 		}
 
+		public static string Percent(double? n, int? precision = 1, bool color = false)
+		{
+			var valid = IsValid(n);
+			if (valid == DataState.valid)
+			{
+				var culture = Options.Culture;
+				if (precision.HasValue)
+					culture.NumberFormat.PercentDecimalDigits = precision.Value;
+
+				var output = n.Value.ToString("p", culture);
+
+				if (color)
+				{
+					var colorClass = n.Value < 0 ? Options.NegativeColorClass : n.Value > 0 ? Options.PositiveColorClass : Options.NeutralColorClass;
+					output = string.Format("<span class=\"{0}\">{1}</span>", colorClass, output);
+				}
+
+				return output;
+			}
+
+			return ErrorString(valid);
+		}
+
 		public static string Currency(double? n, int? precision = 2, bool color = false)
 		{
 			var valid = IsValid(n);
